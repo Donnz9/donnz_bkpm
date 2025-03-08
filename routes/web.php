8ae17e6893d235b1fb8d67c9a2d907f66e92f911
Donnz9 <donihermawwan@gmail.com>
2025-03-08 10:43:30 +0700

    acara 8

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Backend\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::resource('/home', HomeController::class);
+Route::resource('/dashboard', DashboardController::class);
