da2947206c44112b02150b9807b46b161b34334e
Donnz9 <donihermawwan@gmail.com>
2025-03-09 22:47:06 +0700

    acara 22

--- app/Http/Controllers/Backend/ApiPendidikanController.php
+++ app/Http/Controllers/Backend/ApiPendidikanController.php
@@ -14,4 +14,41 @@ class ApiPendidikanController extends Controller
         $pendidikan = Pendidikan::all();
         return Response::json($pendidikan, 201);
     }
+
+    public function getPen($id)
+    {
+        $pendidikan = Pendidikan::find($id);
+
+        return Response::json($pendidikan, 200);
+    }
+
+    public function createPen(Request $request)
+    {
+        Pendidikan::create($request->all());
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil ditambahkan!'
+        ], 201);
+    }
+
+    public function updatePen($id, Request $request)
+    {
+        Pendidikan::find($id)->update($request->all());
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil dirubah!'
+        ], 201);
+    }
+
+    public function deletePen($id)
+    {
+        Pendidikan::destroy($id);
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil dihapus!'
+        ], 201);
+    }
 }
